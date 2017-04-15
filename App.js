f4c051c08e7fbc747f6614559dfec6b81d73a2db
Sterling Cobb <sterlingcobb@gmail.com>
2017-04-14 18:10:47 -0600

    add simple template for loading

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AddressBook from './src/screens/address_book'
+import { AppLoading } from 'expo'
 
-export default class App extends React.Component {
+export default class App extends Component {
+
+  state = {
+    isLoading: true,
+  };
+
   render() {
+    if (this.state.isLoading) {
+      return <AppLoading />
+    }
+
     return (
       <AddressBook />
     );
