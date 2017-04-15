c0cce3b0353bccd19a3075aaa4b48ae2c87dc578
Sterling Cobb <sterlingcobb@gmail.com>
2017-04-14 19:03:30 -0600

    add some basic styles

--- src/screens/address_book/index.js
+++ src/screens/address_book/index.js
@@ -12,13 +12,20 @@ export default class AddressBook extends Component {
       <ListView 
         dataSource = { this.dataSourceWrapper(this.props.contacts) }
         renderRow = { this.renderRow }
+        renderSeparator = { this.renderSeparator }
       />
     );
   }
 
   renderRow (contact) {
     return (
-      <Text>{ contact.name }</Text>
+      <Text style = {styles.contactName}>{ contact.name }</Text>
+    )
+  }
+
+  renderSeparator () {
+    return (
+      <View style = {styles.separator} />
     )
   }
 
@@ -35,4 +42,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  separator: {
+    backgroundColor: 'lightgray',
+    height: 1
+  },
+  contactName: {
+    padding: 10,
+    fontSize: 23
+  }
 });
