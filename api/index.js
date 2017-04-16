eb5c1ed8abd0fb4dc6dd617cdde4edf82b0495e9
Sterling Cobb <sterlingcobb@gmail.com>
2017-04-16 14:19:05 -0600

    add uri helpers and start archetecting how URI's will work

--- api/index.js
+++ api/index.js
@@ -1,1 +1,21 @@
 import { API_TOKEN } from 'react-native-dotenv'
+import { BASE_URL } from 'react-native-dotenv'
+import URI from 'urijs'
+
+
+/**********************************************
+ * Handle creating a easily usable uri. Not
+ * implementing pagination but if I was, I would 
+ * allow for a "default" param attached to the uri
+ * that would represent the page/per_page options.
+ * *******************************************/
+
+const uri = (resource, defaultParams = {token: API_TOKEN, page: 1, per: 100}) => {
+  const uri = new URI(BASE_URL)
+  return uri.segment(resource).addQuery(defaultParams)
+}
+
+export const getContats () => {
+  const contactsUri = uri('contacts')
+  return contactsUri.toString()
+}
